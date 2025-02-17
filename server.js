1ddb799bd2b5f59746a6fe39e2c9bed82969e94a
Armando <72764913+Armando284@users.noreply.github.com>
2025-02-17 09:01:28 -0500

    test: try winston for loggin

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const app = require('./app')
 const env = require('./config/env')
-const debug = require('debug')('DEV:AI_DRIVEN_API')
+// const debug = require('debug')('DEV:AI_DRIVEN_API')
+const winston = require('winston')
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/combined.log' })
+  ]
+});
 
-debug('Starting %o', 'DEV:AI_DRIVEN_API')
+// debug('Starting %o', 'DEV:AI_DRIVEN_API')
 app.listen(env.PORT, () => {
-  debug(`Server is running on port: ${env.PORT}`)
+  // debug(`Server is running on port: ${env.PORT}`)
+  logger.info(`Server is running on port: ${env.PORT}`)
 })
