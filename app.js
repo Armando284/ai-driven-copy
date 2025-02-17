a802c9a1e4e27875ecd2ca307703946eef828b22
Armando <72764913+Armando284@users.noreply.github.com>
2025-02-17 16:02:25 -0500

    feat: adds helmet security

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
 const claimLetterRoutes = require('./src/modules/claim-letter/routes/claim-letter.route')
+
 
 const app = express()
 
+app.use(helmet())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api/claim-letter', claimLetterRoutes)
